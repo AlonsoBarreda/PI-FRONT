20dd0217eda324b988a8f1b4e4e11d6d6c99a8b7
julca95 <gianjulca.5@hotmail.com>
2023-10-22 11:53:00 -0500

    avance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AddUsuarioComponent } from './components/add-usuario/add-usuario.component';
 import { AddPrestamistaComponent } from './components/add-prestamista/add-prestamista.component';
 import { AddJefePrestamistaComponent } from './components/add-jefe-prestamista/add-jefe-prestamista.component';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
@@ -14,7 +19,14 @@ import { AddJefePrestamistaComponent } from './components/add-jefe-prestamista/a
     AddJefePrestamistaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    AppComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AppSettings } from '../app.settings';
+import { Usuario } from '../models/usuario.model';
+
+const url=AppSettings.API_ENDPOINT+'/util'
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
 
-  constructor() { }
+  constructor(private http: HttpClient ) { }
+
+  actualiza(obj:Usuario):Observable<any>{
+    return this.http.put(url+ "/actualizaUser", obj);
+}
 }
